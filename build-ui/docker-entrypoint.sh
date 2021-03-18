cf254d8c1784482364387e798a6df78afad138fc
Andrew Redden <opensource@andrewredden.com>
2021-03-18 06:12:09 -0300

    export env variables before booting wallet

--- build-ui/docker-entrypoint.sh
+++ build-ui/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REACT_APP_BASE_SIGNING_URL=relay.lattice-connect.public.dappnode
-REACT_APP_GRIDPLUS_CLOUD_API=relay.lattice-connect.public.dappnode:3000
+export REACT_APP_BASE_SIGNING_URL=relay.lattice-connect.public.dappnode
+export REACT_APP_GRIDPLUS_CLOUD_API=relay.lattice-connect.public.dappnode:3000
 
 bash -c "serve -s build -l ${WEB_APP_LADDR:-3000}"
